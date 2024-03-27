b6186e1ca636721fbff44fe08c3f579c1c3ce580
Ivan Petkov <skokpodskok@gmail.com>
2024-03-27 18:01:45 +0200

    add some text to Settings screen

--- app/src/main/java/com/example/gol/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/gol/ui/screens/settings/SettingsScreen.kt
@@ -1,6 +1,8 @@
 package com.example.gol.ui.screens.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,14 +13,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gol.logic.GolStarter
 
 @Composable
 fun SettingsScreen(onBackPressed: ()-> Unit, viewModel: SettingsViewModelInterface) {
-    Column(horizontalAlignment = Alignment.End) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp, top = 16.dp),
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            text = "Select Pattern"
+        )
         LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             items(viewModel.starters) {
@@ -34,7 +50,7 @@ fun SettingsScreen(onBackPressed: ()-> Unit, viewModel: SettingsViewModelInterfa
         }
         Button(
             onClick = { onBackPressed() },
-            modifier = Modifier.padding(top = 12.dp),
+            modifier = Modifier.padding(bottom = 16.dp, top = 16.dp),
             colors = ButtonDefaults.buttonColors().copy(containerColor = Color.Black)
         ) {
             Text(text = "Cancel")
